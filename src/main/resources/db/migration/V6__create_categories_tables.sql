ae930e05240ca9aef43c12a8a441cae0bba2016a
AbdelRhman Samy <68971771+vemonser@users.noreply.github.com>
2026-05-30 23:32:16 +0300

    Update V6__create_categories_tables.sql

--- src/main/resources/db/migration/V6__create_categories_tables.sql
+++ src/main/resources/db/migration/V6__create_categories_tables.sql
@@ -2,8 +2,7 @@ CREATE TABLE
     categories (
         id BIGSERIAL PRIMARY KEY,
         slug VARCHAR(120) NOT NULL UNIQUE,
-        parent_id BIGINT REFERENCES
-        categories (id) ON DELETE SET NULL,
+        parent_id BIGINT REFERENCES categories(id) ON DELETE SET NULL,
         is_active BOOLEAN NOT NULL DEFAULT TRUE,
         created_at TIMESTAMP NOT NULL,
         updated_at TIMESTAMP NOT NULL,
@@ -13,8 +12,7 @@ CREATE TABLE
 CREATE TABLE
     category_translations (
         id BIGSERIAL PRIMARY KEY,
-        category_id BIGINT NOT NULL REFERENCES
-        categories (id) ON DELETE CASCADE,
+        category_id BIGINT NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
         language_code VARCHAR(2) NOT NULL, 
         name VARCHAR(100) NOT NULL,
         description VARCHAR(500),
